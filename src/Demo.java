ab70d9b66099ad79f422750babc4b1f19c19de9c
Antriksh <antriksh.kumar13@gmail.com>
2019-06-18 22:10:25 +1200

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\antri\\Downloads\\Softwares\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		//driver.navigate("http://www.google.com");
+		System.out.println("Hello");
+
+	}
+
+}
