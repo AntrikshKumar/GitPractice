050d09fdd54ac8fb2473f22dc5269d82d611354d
Antriksh <antriksh.kumar13@gmail.com>
2019-06-18 22:58:48 +1200

    Added 2 new lines in Demo class

--- src/Demo.java
+++ src/Demo.java
@@ -11,6 +11,8 @@ public class Demo {
 		driver.manage().window().maximize();
 		//driver.navigate("http://www.google.com");
 		System.out.println("Hello");
+		System.out.println("Hello1");
+		System.out.println("Hello3");
 
 	}
 
